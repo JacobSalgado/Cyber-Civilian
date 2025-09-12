ee3aeae8b238221a3851eb0163b08d7c80a30bca
unknown <homa130702@gmail.com>
2025-09-12 16:32:54 -0700

    Player Movement

--- Assets/Entities/Player/Player.cs
+++ Assets/Entities/Player/Player.cs
@@ -1,16 +1,39 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
+
+    public float moveSpeed = 5f;
+    public Rigidbody2D rb;
+    Vector2 movement;
+    public InputAction moveAction;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
+    }
+
+    void onEnable()
+    {
+        moveAction.Enable();
+    }
+
+    void onDisable()
+    {
+        moveAction.Disable();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        movement = moveAction.ReadValue<Vector2>();
+    }
+
+    //Called once per physics time step    
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
     }
 }
