99f98a04eed9400e46698d8bf0dd3a71f6444c50
mevwin <vielgradude999@gmail.com>
2025-09-17 22:32:32 -0400

    implemented sample SFX for player

--- Assets/Entities/Player/Player.cs
+++ Assets/Entities/Player/Player.cs
@@ -1,9 +1,3 @@
-using System.Collections.Generic;
-using NUnit.Framework;
-using Unity.VisualScripting;
-using UnityEngine;
-using UnityEngine.AI;
-using UnityEngine.Audio;
 using UnityEngine.InputSystem;
 
 public class Player : Entity
@@ -20,67 +14,10 @@ public class Player : Entity
     void Start()
     {
         InitializeStates();
-        //current_state = stateMap["Idle"];
-    }
-
-    void Update()
-    {
-        //velocity = moveAction.action.ReadValue<Vector2>();
     }
 
     void FixedUpdate()
     {
         current_state.UpdateState();
     }
-
-    /*
-    private enum SFXNames
-    {
-        STEP1,
-        STEP2,
-        STEP3
-    }
-
-    public InputActionReference moveAction;
-    private float deltaCount = 0f;
-    private int stepCounter = 0;
-
-    public Vector2 velocity;
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-    }
-
-    void PlayFootsteps(float delta)
-    {
-        if (deltaCount > stepSFXDuration || deltaCount == 0f)
-        {
-            SFXPlayer.PlayOneShot(entityData.SFX[stepCounter]);
-            deltaCount = 0f;
-            stepCounter++;
-            if (stepCounter % 3 == 0)
-                stepCounter = 0;
-        }
-        deltaCount += delta;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        velocity = moveAction.action.ReadValue<Vector2>();
-    }
-
-    // Called once per physics time step    
-    void FixedUpdate()
-    {
-        rb.linearVelocity = new Vector2(velocity.x * entityData.moveSpeed, velocity.y * entityData.moveSpeed);
-        if (rb.linearVelocity != Vector2.zero)
-            PlayFootsteps(Time.deltaTime);
-        else
-        {
-            deltaCount = 0f;
-        }
-    }
-    */
 }
--- Assets/Entities/Player/PlayerMove.cs
+++ Assets/Entities/Player/PlayerMove.cs
@@ -1,34 +1,52 @@
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class PlayerMove : State
 {
     Vector2 velocity;
     Player player;
+    private float deltaCount = 0f;
+    private float timeToStep = 0.35f;
+    int stepCounter = 0;
 
     public PlayerMove(Entity new_entity) : base(new_entity)
     {
-        player = (Player) new_entity;
+        player = (Player)new_entity;
+    }
+
+    private void PlayFootsteps()
+    {
+        if (deltaCount > timeToStep)
+        {
+            player.SFXPlayer.PlayOneShot(player.entityData.SFX[stepCounter++]);
+            deltaCount = 0f;
+            if (stepCounter > 2) stepCounter = 0;
+        }
     }
 
     public override void EnterState(Dictionary<string, object> args = null)
     {
-        
+        deltaCount = 0f;
+        stepCounter = 0;
+        player.SFXPlayer.PlayOneShot(player.entityData.SFX[stepCounter++]);
     }
 
     public override void UpdateState()
     {
         velocity = player.moveAction.action.ReadValue<Vector2>();
 
-        if (player.moveAction.action.ReadValue<Vector2>() != Vector2.zero)
+        if (velocity != Vector2.zero)
+        {
+            deltaCount += Time.deltaTime;
             entity.rb.linearVelocity = new Vector2(velocity.x * entity.entityData.moveSpeed, velocity.y * entity.entityData.moveSpeed);
-        else
-            entity.ChangeState("Idle");
+            PlayFootsteps();
+        }
+        else entity.ChangeState("Idle");
     }
 
     public override void ExitState(Dictionary<string, object> args = null)
     {
-        
+
     }
+
 }
