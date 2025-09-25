9f08d92899260b25caf11465e93c62f5b04f6b54
unknown <homa130702@gmail.com>
2025-09-25 07:39:38 -0700

    Added turning to mouse pointer

--- Assets/Entities/Player/Player.cs
+++ Assets/Entities/Player/Player.cs
@@ -1,8 +1,13 @@
+//using System.Numerics;
+using Unity.VisualScripting;
+using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class Player : Entity
 {
+    public Camera cam;
     public InputActionReference moveAction;
+    Vector2 mousePos;
 
     public override void InitializeStates()
     {
@@ -17,8 +22,17 @@ public class Player : Entity
         InitializeStates();
     }
 
+    void Update()
+    {
+        mousePos = cam.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+    }
+
     void FixedUpdate()
     {
         current_state.UpdateState();
+
+        Vector2 lookDir = mousePos - rb.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = angle;
     }
 }
