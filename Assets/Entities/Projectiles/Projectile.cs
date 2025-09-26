47993564f76b55e384dfba01d7b06a5419ce5e85
unknown <homa130702@gmail.com>
2025-09-26 15:52:39 -0700

    Shooting effect

--- Assets/Entities/Projectiles/Projectile.cs
+++ Assets/Entities/Projectiles/Projectile.cs
@@ -5,10 +5,10 @@ public class Projectile : MonoBehaviour
     public GameObject hitEffect;
     [SerializeField] public float damage = 1f;
 
-    void OnCollissionEnter2D(Collision2D collision)
+    void OnCollisionEnter2D(Collision2D collision)
     {
         GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
-        Destroy(effect, 5f);
+        Destroy(effect, 0.1f);
         Destroy(gameObject);
     }
     
