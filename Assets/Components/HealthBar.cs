74c1206d850bd14bdbdf0ec4faef7fca599163db
JacobSalgado <153098612+JacobSalgado@users.noreply.github.com>
2025-10-03 18:13:07 -0400

    changes

--- Assets/Components/HealthBar.cs
+++ Assets/Components/HealthBar.cs
@@ -6,12 +6,6 @@ public class HealthBar : MonoBehaviour
     public EntityData entityData;
     public Slider slider;
 
-<<<<<<< HEAD
-    public GameObject enemy;
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-=======
     public HealthBar(EntityData entityData, Slider slider)
     {
         this.entityData = entityData;
@@ -21,24 +15,8 @@ public class HealthBar : MonoBehaviour
     }
 
     public void UpdateHealthBar()
->>>>>>> dev
     {
         slider.maxValue = entityData.maxHealth;
         slider.value = entityData.currentHealth;
     }
-<<<<<<< HEAD
-
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-        slider.maxValue = entityData.maxHealth;
-        slider.value = entityData.currentHealth;
-    }
-
-    public void TakeDamage(int amount)
-    {
-        slider.value -= amount;
-    }
-=======
->>>>>>> dev
 }
