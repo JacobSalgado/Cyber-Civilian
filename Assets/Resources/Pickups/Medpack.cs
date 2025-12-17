24b83d72747d614f3d9cd2db10f4d3f15f3b510c
unknown <vielgradude999@gmail.com>
2025-12-16 21:46:27 -0500

    fixed medpack null error

--- Assets/Resources/Pickups/Medpack.cs
+++ Assets/Resources/Pickups/Medpack.cs
@@ -4,22 +4,10 @@ using UnityEngine;
 public class Medpack : PickupItem
 {
     [SerializeField] private int healAmount = 500;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     public void OnTriggerEnter2D(Collider2D collision)
     {
-        collision.gameObject.TryGetComponent<Player>(out var player);
-        if (player.playerData.currentHealth < player.playerData.maxHealth) {
+        if (collision.gameObject.TryGetComponent<Player>(out var player) && player.playerData.currentHealth < player.playerData.maxHealth) {
             player.SetHealth(player.playerData.currentHealth + healAmount);
             Destroy(gameObject);
         }
