3120e4bc2e993705cfa339a4fd4479f2b9ad34a8
unknown <vielgradude999@gmail.com>
2025-12-17 01:59:13 -0500

    tutorial level finished

--- Assets/Resources/Pickups/PickupItem.cs
+++ Assets/Resources/Pickups/PickupItem.cs
@@ -6,18 +6,6 @@ public class PickupItem : MonoBehaviour
     // Object lifecycle variables
     [SerializeField] private float lifeTime = 100f;
     [NonSerialized] private float lifeTimer = 0f;
-    
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     public virtual void FixedUpdate()
     {
